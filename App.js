3fe7fe4b3e4dc5569882b9fb159bdb3300e41ac3
REXNICE <ernestrex1996@gmail.com>
2023-10-18 17:22:43 +0100

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, } from 'react-native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import {NavigationContainer} from '@react-navigation/native'
+import { Login, Signup, Welcome } from './screens';
 
+const Stack = createNativeStackNavigator()
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName='welcome'
+            >
+                <Stack.Screen
+                    name='welcome'
+                    component={Welcome}
+                    options ={{
+                        headerShown:false
+                    }}
+                />
+                <Stack.Screen
+                    name='Login'
+                    component={Login}
+                    options ={{
+                        headerShown:false
+                    }}
+                />
+                <Stack.Screen
+                    name='Signup'
+                    component={Signup}
+                    options ={{
+                        headerShown:false
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+         
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
