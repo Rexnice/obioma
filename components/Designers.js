622002408cc443df45141732f0d6446c83a1f414
Rexnice <51068031+Rexnice@users.noreply.github.com>
2024-01-13 22:22:15 +0100

    "13012024 commit"

--- components/Designers.js
+++ components/Designers.js
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import designers from '../data/designers';
 
 const Designers = () => {
-  const data = designers
+  const data = designers;
   return (
-    <View>
-      <Text>Designers</Text>
+    <View style={{margin:10}}>
+      <Text style={{fontSize:16, fontWeight:'500'}}>Meet Amazing Designers</Text>
+      <ScrollView>
+
+      </ScrollView>
     </View>
   )
 }
